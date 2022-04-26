4659b5704f3d87a37f5be1ba079846739f8f4441
Dora Koncz <adriendorcsi@yahoo.com>
2022-04-26 17:43:07 +0200

    routing

--- YatchChallenge/src/app/app-routing.module.ts
+++ YatchChallenge/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InOffertaComponent } from './in-offerta/in-offerta.component';
+import { LoginComponent } from './login/login.component';
+import { MioNoleggioComponent } from './mio-noleggio/mio-noleggio.component';
+import { TutteComponent } from './tutte/tutte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'tutte', component:TutteComponent},
+  {path: 'in-offerta', component:InOffertaComponent},
+  {path: 'mio-noleggio', component:MioNoleggioComponent},
+  {path: 'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
