78d6643748df0a1c27919dc8c8bb71d5a65bca7a
giuseppe <giuseppe.garone@edu.immaginazioneelavoro.it>
2022-05-04 16:33:19 +0200

    Update offerte.component.ts

--- YatchChallenge/src/app/home/offerte/offerte.component.ts
+++ YatchChallenge/src/app/home/offerte/offerte.component.ts
@@ -18,13 +18,8 @@ export class OfferteComponent implements OnInit {
   }
   
   ngOnInit(): void {
-<<<<<<< Updated upstream
     this.yatchService.getYatchOfferta().subscribe((yatch)=>{
       this.yatchs = yatch;
-=======
-    this.yatchService.getAll().subscribe((yatchs)=>{
-      this.yatchs = yatchs;
->>>>>>> Stashed changes
     });
     console.log(this.yatchs);
     
