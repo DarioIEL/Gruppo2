08d621dfcd34174a03bc3c6d7e8aa681ecfc2109
Alexis <alexistaveras97@gmail.com>
2022-05-05 17:06:35 +0200

    Autenticazione + altre robe
    Si vede chi ha loggato. Il logout funziona  ma bisogna ricaricare la pagina

--- YatchChallenge/src/app/app-routing.module.ts
+++ YatchChallenge/src/app/app-routing.module.ts
@@ -7,11 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { MioNoleggioComponent } from './mio-noleggio/mio-noleggio.component';
 import { TutteComponent } from './tutte/tutte.component';
 
+import { AuthGuard } from './auth.guard';
+import { NavComponent } from './nav/nav.component';
+
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'tutte', component:TutteComponent},
   {path: 'in-offerta', component:InOffertaComponent},
   {path: 'mio-noleggio', component:MioNoleggioComponent},
+  {path: 'nav', component: NavComponent, canActivate: [AuthGuard]},
   {path: 'login', component:LoginComponent},
   {path: 'acquisto/:id', component: AcquistoComponent}
 ];
--- YatchChallenge/src/app/auth.service.ts
+++ YatchChallenge/src/app/auth.service.ts
@@ -1,20 +1,78 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import * as auth from 'firebase/auth';
 import {AngularFireAuth} from '@angular/fire/compat/auth';
-import {GoogleAuthProvider} from 'firebase/auth'
+import {GoogleAuthProvider} from 'firebase/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+userData : any;
   constructor(
-    public afAuth : AngularFireAuth
-  ) { }
-
-
-//
-SignIn (email, password) {
-  return this.afAuth
+    public afs: AngularFirestore, //Firestore Service
+    public afAuth : AngularFireAuth, //Firebase auth service
+    public router : Router,
+    public ngZone : NgZone
+  ) { 
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
+  
+  
+  
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null && user.emailVerified !== false ? true : false;
+  }
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+  
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+  
+  
+  
+  //
+  SignIn (email, password) {
+    return this.afAuth
   .signInWithEmailAndPassword(email, password)
   .then((result) => {
     console.log(result);
@@ -48,10 +106,33 @@ PopupLogin(provider) {
 Logout() {
   return this.afAuth.signOut().then(() => {
     window.alert('Hai effettuato il logout con successo!');
-
+    
   });
 }
 
+
+
+
+
+  SendVerificationMail() {
+    return this.afAuth.currentUser
+      .then((u: any) => u.sendEmailVerification())
+      .then(() => {
+        this.router.navigate(['verify-email-address']);
+      });
+    }
+
+
+    ForgotPassword(passwordResetEmail: string) {
+      return this.afAuth
+        .sendPasswordResetEmail(passwordResetEmail)
+        .then(() => {
+          window.alert('Password reset email sent, check your inbox.');
+        })
+        .catch((error) => {
+          window.alert(error);
+        });
+      }
 
 
 
