4d36cff4fb53cae4280fa0bd51547bc9af247ff5
dora <adriendorcsi@yahoo.com>
2022-05-06 16:09:54 +0200

    sistemato la pagina add

--- YatchChallenge/src/app/home/home.component.ts
+++ YatchChallenge/src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   constructor(private yatchService: YatchService) { }
 
   ngOnInit(): void {
-    console.log(this.yatchService.getYatchOfferta());
+    // console.log(this.yatchService.getYatchOfferta());
     
   }
 
